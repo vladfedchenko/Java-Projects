c9c2a424c1a41469634f9411227f586905cab85f
Vladyslav Fedchenko <vladfedchenko@gmail.com>
2016-05-21 18:27:18 +0300

    Spring support added

--- hibersite/src/main/java/vladfedchenko/lab/App.java
+++ hibersite/src/main/java/vladfedchenko/lab/App.java
@@ -2,6 +2,8 @@ package vladfedchenko.lab;
 
 import vladfedchenko.lab.dao.GeneralDAO;
 import vladfedchenko.lab.dbclasses.Artist;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.sql.Date;
 
@@ -9,10 +11,10 @@ public class App
 {
     public static void main( String[] args )
     {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("Beans.xml");
 
-        System.out.println( "Hello World!" );
-
-        GeneralDAO dao = new GeneralDAO();
+        GeneralDAO dao = (GeneralDAO) context.getBean("generalDao");
 
         //dao.addArtist("Frank Sinatra", Date.valueOf("1915-12-12"));
         Artist artist = dao.getArtist(new Integer(1));
